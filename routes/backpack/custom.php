77c2334cd1f9ad75aad845c755b8fdcc3f750962
Владимир Василев <95617458+rMacBG@users.noreply.github.com>
2023-01-08 15:48:18 +0200

    c

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -18,4 +18,5 @@ Route::group([
 ], function () { // custom admin routes
     Route::crud('user', 'UserCrudController');
     Route::crud('article', 'ArticleCrudController');
+    Route::crud('tag', 'TagCrudController');
 }); // this should be the absolute last line of this file
